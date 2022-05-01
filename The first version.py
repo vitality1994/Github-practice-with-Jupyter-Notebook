1369c611d76188514dcb10c9be99ba5956ad47c4
Jooyong <jylee941@gmail.com>
2022-05-01 17:59:44 +0900

    commit after making py file of the first version

---
+++ The first version.py
@@ -0,0 +1,17 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.13.8
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+print("Hello This is the first version")
+print("I added this line for the first update!")
