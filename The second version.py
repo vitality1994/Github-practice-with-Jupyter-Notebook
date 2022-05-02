c1d1364f5e27abd71a0cc0bbaa9db60980903dea
Jooyong <jylee941@gmail.com>
2022-05-02 15:03:22 +0900

    the second update for the second version + py file of the second version

---
+++ The second version.py
@@ -0,0 +1,18 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.13.8
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+print("This is the second version!")
+print("I added this for the first update!")
+print("the second update for the second version!")
